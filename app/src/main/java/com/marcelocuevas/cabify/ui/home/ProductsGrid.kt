1d78540d61c2132cef966b12f046c8aaec322e5e
Marcelo Cuevas <jmarcecuevas@gmail.com>
2023-04-17 12:11:08 -0300

    Add Progress Indicator in Loading mode

--- app/src/main/java/com/marcelocuevas/cabify/ui/home/ProductsGrid.kt
+++ app/src/main/java/com/marcelocuevas/cabify/ui/home/ProductsGrid.kt
@@ -1,12 +1,13 @@
 package com.marcelocuevas.cabify.ui.home
 
-import android.content.res.Configuration
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.PullRefreshState
@@ -15,9 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.marcelocuevas.cabify.data.model.Product
 import com.marcelocuevas.cabify.ui.theme.CabifyTheme
 
 internal val gradientWidth
@@ -40,7 +39,15 @@ fun ProductsGrid(
 ){
     when (uiState) {
         is HomeUiState.Loading -> {
-
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    color = CabifyTheme.colors.brand,
+                    strokeWidth = 3.dp
+                )
+            }
         }
         is HomeUiState.Success -> {
             Box(
