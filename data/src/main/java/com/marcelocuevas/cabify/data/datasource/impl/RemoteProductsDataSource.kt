d923eb67247bf979c00f955dbf070609bf2421c7
Marcelo Cuevas <jmarcecuevas@gmail.com>
2023-04-18 03:15:51 -0300

    Catch exception in http request

--- data/src/main/java/com/marcelocuevas/cabify/data/datasource/impl/RemoteProductsDataSource.kt
+++ data/src/main/java/com/marcelocuevas/cabify/data/datasource/impl/RemoteProductsDataSource.kt
@@ -3,7 +3,6 @@ package com.marcelocuevas.cabify.data.datasource.impl
 import com.marcelocuevas.cabify.data.api.ProductItemDTO
 import com.marcelocuevas.cabify.data.api.ProductsAPI
 import com.marcelocuevas.cabify.data.datasource.ProductsDataSource
-import java.io.IOException
 import javax.inject.Inject
 
 class RemoteProductsDataSource @Inject constructor(
@@ -13,7 +12,7 @@ class RemoteProductsDataSource @Inject constructor(
     override suspend fun getProducts(): List<ProductItemDTO> {
         return try {
             productsAPI.getProducts().products
-        } catch (e: IOException) {
+        } catch (e: Exception) {
             emptyList()
         }
     }
